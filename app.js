855a4fd94f9907035b6229d36744f4d4108cc016
jor-eldevera <jorell.devera@gmail.com>
2025-03-03 19:00:14 -0800

    feat: Add sign-up form

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const bcrypt = require("bcryptjs");
 const pool = new Pool({
     host: process.env.PG_HOST,
     user: process.env.PG_USER,
-    database: "top_auth_basics",
-    password: "115326",
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT
 });
 
@@ -26,7 +26,19 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
     res.render("index", { user: req.user });
 });
+app.get("/log-in", (req, res) => res.render("log-in-form"));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+    try {
+        const { username, password, first_name, last_name } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query("INSERT INTO users (username, password, first_name, last_name) VALUES ($1, $2, $3, $4)", [username, hashedPassword, first_name, last_name]);
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        return next(err)
+    }
+});
 
 // makes the currentUser variable available in all of the views
 app.use((req, res, next) => {
