551256bc2534b90417d9a48f4f4a6fa3c2eb665a
jor-eldevera <jorell.devera@gmail.com>
2025-03-03 19:52:30 -0800

    feat: Add a login form with error message capability

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
     res.render("index", { user: req.user });
 });
-app.get("/log-in", (req, res) => res.render("log-in-form"));
+app.get("/log-in", (req, res) => {
+    const messages = req.session.messages || [];
+    req.session.messages = [];
+    res.render("log-in-form", { message: messages[messages.length - 1] });
+});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
     try {
@@ -39,11 +43,63 @@ app.post("/sign-up", async (req, res, next) => {
         return next(err)
     }
 });
+app.post(
+    "/log-in",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/log-in",
+        failureMessage: true
+    })
+);
+app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    })
+});
 
 // makes the currentUser variable available in all of the views
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     next();
-})
+});
+
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+        const user = rows[0];
+
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
