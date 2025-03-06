3667e9afd650b720d85dbbbcb5286fb89941892b
jor-eldevera <jorell.devera@gmail.com>
2025-03-06 13:45:38 -0800

    feat: Add basic styling

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
